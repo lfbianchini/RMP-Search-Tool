c198d1091a911a3c588660b38ca1f1230c589b50
lfbianchini <lfbianchini@dons.usfca.edu>
2024-06-04 21:01:26 -0700

    added cookie injection to getprofessorid and more simple selector paths to getuniversityid and getprofessorid for better performance, also created getprofessorrating

--- src/demo/src/main/java/com/searchmaster/App.java
+++ src/demo/src/main/java/com/searchmaster/App.java
@@ -2,14 +2,19 @@ package com.searchmaster;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Set;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Attributes;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriver.Options;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 
@@ -21,9 +26,11 @@ public class App {
             String query = "https://www.ratemyprofessors.com/search/schools?q=" + clean; //format url
 
             Document page = Jsoup.connect(query).timeout(3000).userAgent("Mozilla/126.0").get(); //mozilla agent to connect to the page
-            Elements pageElements = page.select("#root > div > div > div:nth-child(3) > div.SearchResultsPage__StyledSearchResultsPage-vhbycj-0.bgplVn");
+            Elements pageElements = page.select("a.SchoolCard__StyledSchoolCard-sc-130cnkk-0:nth-child(1)");
+            //#root > div > div > div:nth-child(3) > div.SearchResultsPage__StyledSearchResultsPage-vhbycj-0.bgplVn > div.SearchResultsPage__SearchResultsWrapper-vhbycj-1.gxbBpy > div:nth-child(3) > a:nth-child(1)
             String id = pageElements.get(0).attributes().get("href"); //gets the id
-            return id;
+            return id.substring(8);
+            
         }
 
 
@@ -35,16 +42,27 @@ public class App {
             String query = "https://www.ratemyprofessors.com/search/professors/" + universityID + "?q=" + clean; //format url
             WebDriver driver = new FirefoxDriver(); //emulates browser w/ selenium
             driver.get(query); //go to the query site
-
-            Actions action = new Actions(driver);
-            action.sendKeys(Keys.ESCAPE).perform(); //hit esc to close out of the modal (popup)
+            Cookie cookie2 = new Cookie("ccpa-notice-viewed-02", "true",".ratemyprofessors.com", "/", null, true, false, "None");
+            driver.manage().addCookie(cookie2);
+            driver.get(query);
+            driver.manage().window().minimize();
 
             Document page = Jsoup.parse(driver.getPageSource()); //hand selenium driver's source back to jsoup
-            Elements pageElements = page.select("#root > div > div > div:nth-child(3) > div.SearchResultsPage__StyledSearchResultsPage-vhbycj-0.bgplVn > div.SearchResultsPage__SearchResultsWrapper-vhbycj-1.gxbBpy > div:nth-child(4) > a:nth-child(1)");
+            Elements pageElements = page.select("a.TeacherCard__StyledTeacherCard-syjs0d-0:nth-child(1)");
+            //a.TeacherCard__StyledTeacherCard-syjs0d-0:nth-child(1)
+            //#root > div > div > div:nth-child(3) > div.SearchResultsPage__StyledSearchResultsPage-vhbycj-0.bgplVn > div.SearchResultsPage__SearchResultsWrapper-vhbycj-1.gxbBpy > div:nth-child(5) > a:nth-child(1)
             // ^ query the html element containing the href for the first professor shown on the page
             String id = pageElements.get(0).attributes().get("href"); //get the ID from the href 
             driver.close();
-            return id;
+            return id.substring(11);
+        }
+
+        public static String getProfessorRating(String professorID) throws IOException {
+            String query = "https://www.ratemyprofessors.com/professor/" + professorID;
+            Document page = Jsoup.connect(query).get();
+            Elements pageElements = page.select("#root > div > div > div.PageWrapper__StyledPageWrapper-sc-3p8f0h-0.lcpsHk > div.TeacherRatingsPage__TeacherBlock-sc-1gyr13u-1.jMpSNb > div.TeacherInfo__StyledTeacher-ti1fio-1.kFNvIp > div:nth-child(1) > div.RatingValue__AvgRating-qw8sqy-1.gIgExh > div > div.RatingValue__Numerator-qw8sqy-2.liyUjw");
+            String rating = pageElements.first().text();
+            return rating;
         }
 
         public static String formatNameForQuery(String name) { //method for formatting names into query form eg "university    of washington" => "university%20of%20washington"
@@ -57,5 +75,6 @@ public class App {
         public static void main(String[] args) throws IOException {
             System.out.println(App.getUniversityID("university of san francisco"));
             System.out.println(App.getProfessorId("1600", "karen bouwer"));
+            System.out.println(getProfessorRating("517854"));
         }
 }
