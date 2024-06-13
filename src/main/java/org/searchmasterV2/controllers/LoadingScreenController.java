f051c5dd0b40dcf4f7cede904e89655358c4830c
luca <lfbianchini@dons.usfca.edu>
2024-06-13 11:42:04 -0700

    fixed loading screen by making LoadingScreenController implement Initializable and also fixed loading_screen.fxml

--- src/main/java/org/searchmasterV2/controllers/LoadingScreenController.java
+++ src/main/java/org/searchmasterV2/controllers/LoadingScreenController.java
@@ -1,14 +1,18 @@
 package org.searchmasterV2.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ProgressIndicator;
 
-public class LoadingScreenController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoadingScreenController implements Initializable {
 
     @FXML
     private ProgressIndicator progressIndicator;
-
-    public void initialize() {
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
         // Initialization code if needed
         progressIndicator.setProgress(ProgressIndicator.INDETERMINATE_PROGRESS);
     }
