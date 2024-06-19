7428e8b454914fa9540c89954e96533b0b20c726
Rohan Shah <66500419+rohanshah35@users.noreply.github.com>
2024-06-19 00:27:17 -0700

    bug fixes

--- src/main/java/org/searchmasterV2/controllers/DataDashboardP4Controller.java
+++ src/main/java/org/searchmasterV2/controllers/DataDashboardP4Controller.java
@@ -5,18 +5,13 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
-import javafx.scene.chart.XYChart;
-import org.searchmasterV2.Loader;
-import org.searchmasterV2.Review;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.List;
 import java.util.ResourceBundle;
 
 import static org.searchmasterV2.App.loadFXML;
 import static org.searchmasterV2.App.stage;
-import static org.searchmasterV2.Professor.sentimentList;
 
 public class DataDashboardP4Controller implements Initializable {
 
@@ -25,14 +20,7 @@ public class DataDashboardP4Controller implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        List<Review> sentiments = Loader.getReviewList();
-        lineChartOne.getXAxis().setLabel("MONTHS (#)");
-        lineChartOne.getYAxis().setLabel("SENTIMENT");
-        XYChart.Series series = new XYChart.Series();
-////        for(Review review : sentimentList) {
-////            series.getData().add(new XYChart.Data("") {
-////            });
-////        }
+
     }
 
     @FXML
