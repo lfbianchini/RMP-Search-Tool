14a4960c6a11fab365fbfc67b76ba9eb2be4f513
Rohan Shah <66500419+rohanshah35@users.noreply.github.com>
2024-06-20 18:25:22 -0700

    P5 changes

--- src/main/java/org/searchmasterV2/controllers/DataDashboardP5Controller.java
+++ src/main/java/org/searchmasterV2/controllers/DataDashboardP5Controller.java
@@ -4,14 +4,9 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
-import javafx.scene.chart.LineChart;
-import javafx.scene.chart.XYChart;
-import org.searchmasterV2.Loader;
-import org.searchmasterV2.Review;
 
 import java.io.IOException;
 import java.net.URL;
-import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 import static org.searchmasterV2.App.loadFXML;
@@ -19,28 +14,8 @@ import static org.searchmasterV2.App.stage;
 
 public class DataDashboardP5Controller implements Initializable {
 
-    @FXML
-    private LineChart<String, Integer> lineChart;
-
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        XYChart.Series<String, Integer> series1 = new XYChart.Series<>();
-        ArrayList<Review> reviews = Loader.getRatings();
-        reviews.sort(Comparator.comparing(Review::getDate));
-        List<String> reviewSentiments = new ArrayList<>();
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-
-        for (Review r : reviews) {
-            String date = r.getDate().format(formatter);
-            List<Long> sentiments = r.getConsolidatedSentiment();
-            if (!sentiments.isEmpty()) {
-                Long sentiment = Collections.max(sentiments);
-                int sentimentStr = sentiments.indexOf(sentiment);
-//                reviewSentiments.add(sentimentStr);
-                series1.getData().add(new XYChart.Data<>(date, sentimentStr));
-            }
-        }
-        lineChart.getData().add(series1);
     }
 
     @FXML
